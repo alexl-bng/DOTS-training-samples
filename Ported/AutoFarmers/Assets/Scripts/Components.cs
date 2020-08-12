10bf73ee7755cebafe81493997bb9c3df9184d81
T.J. Martin <tjmartin@bungie.com>
2020-08-12 10:18:03 -0700

    Added some components and tiny scaffold for AI intent

---
+++ Ported/AutoFarmers/Assets/Scripts/Components.cs
@@ -0,0 +1,16 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+public struct Farmer : IComponentData { }
+public struct Drone : IComponentData { }
+
+public struct AI_Sell : IComponentData { }
+public struct AI_Plant : IComponentData { }
+public struct AI_Plow : IComponentData { }
+public struct AI_Break : IComponentData { }
+public struct AI_None : IComponentData { }
+
+public struct RandomNumberGenerator : IComponentData
+{
+	public Random rng;
+}
