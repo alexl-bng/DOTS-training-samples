92a4c7f96646ee4a8eac2b162479f891ea761aac
Peter Lu <peter.lu@unity3d.com>
2020-08-12 15:56:52 -0400

    Initial commit of movement system and path component

---
+++ Ported/AutoFarmers/Assets/Scripts/DroneMovementSystem.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+public class DroneMovementSystem : SystemBase
+{
+    private EntityCommandBufferSystem m_ecb;
+
+    protected override void OnCreate()
+    {
+        m_ecb = World.GetExistingSystem<EndSimulationEntityCommandBufferSystem>();
+    }
+
+    protected override void OnUpdate()
+    {
+        var ecb = m_ecb.CreateCommandBuffer();
+        var deltaTime = Time.DeltaTime;
+        
+        Entities
+            .WithName("drone_movement")
+            .WithAll<Drone, Path>()
+            .WithoutBurst()
+            .ForEach((Entity entity, Path path, in LocalToWorld ltw) =>
+                {
+                    // Drones make a direct bee line to their target
+                    // TODO: account for smoothing
+                    if (math.abs(path.targetPosition.x - ltw.Position.x) > 0.01 ||
+                        math.abs(path.targetPosition.z - ltw.Position.z) > 0.01)
+                    {
+                        ecb.AddComponent(entity,  new Translation()
+                        {
+                            Value = Vector3.MoveTowards(ltw.Position, path.targetPosition, path.speed * deltaTime)
+                        });
+
+                        var distanceFromSource = Vector3.Distance(path.sourcePosition, path.targetPosition);
+                        path.progress = Vector3.Distance(ltw.Position, path.targetPosition) / distanceFromSource * 100.0f;
+                        
+                        // draw a straight line from the current position to the target
+                        Debug.DrawLine(new Vector3(ltw.Position.x, 0.1f, ltw.Position.z),
+                            new Vector3(path.targetPosition.x, 0.1f, path.targetPosition.z), Color.red);
+                    }
+                }).Run();
+    }
+}
---
+++ Ported/AutoFarmers/Assets/Scripts/FarmerMovementSystem.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+public class FarmerMovementSystem : SystemBase
+{
+    private EntityCommandBufferSystem m_ecb;
+
+    protected override void OnCreate()
+    {
+        m_ecb = World.GetExistingSystem<EndSimulationEntityCommandBufferSystem>();
+    }
+    
+    protected override void OnUpdate()
+    {
+        var deltaTime = Time.DeltaTime;
+        var ecb = m_ecb.CreateCommandBuffer();
+
+        Entities
+            .WithName("farmer_movement")
+            .WithAll<Farmer, Path>()
+            .WithoutBurst()
+            .ForEach((Entity entity, Path path, in LocalToWorld ltw) =>
+            {
+                // TODO: account for smoothing
+                if (math.abs(path.targetPosition.x - ltw.Position.x) > 0.01)
+                {    // move along X
+                    ecb.AddComponent(entity, new Translation()
+                    {
+                        Value = ltw.Position + new float3(path.targetPosition.x > ltw.Position.x ? 1f : -1f, 0, 0)
+                    });
+                }
+                else if (math.abs(path.targetPosition.z - ltw.Position.z) > 0.01) 
+                {    // move along Z
+                    ecb.AddComponent(entity, new Translation()
+                    {
+                        Value = ltw.Position + new float3(0, 0, path.targetPosition.z > ltw.Position.z ? 1f : -1f)
+                    });    
+                }
+                
+                var distanceFromSource = Vector3.Distance(path.sourcePosition, path.targetPosition);
+                path.progress = Vector3.Distance(ltw.Position, path.targetPosition) / distanceFromSource * 100.0f;
+                
+                // draw a straight line from the current position to the target
+                Debug.DrawLine(new Vector3(ltw.Position.x, 0.1f, ltw.Position.z),
+                    new Vector3(path.targetPosition.x, 0.1f, path.targetPosition.z), Color.red);
+            }
+        ).Run();
+    }
+}
