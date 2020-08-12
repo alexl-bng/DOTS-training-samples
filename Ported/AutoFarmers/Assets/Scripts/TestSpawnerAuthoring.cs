d8021976269eb896e8693d77ed7d5ba5c53318a0
Alex Lawson <alexl@bungie.com>
2020-08-12 09:48:10 -0700

    add test spawner system

---
+++ Ported/AutoFarmers/Assets/Scripts/TestSpawnerAuthoring.cs
@@ -0,0 +1,10 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+[GenerateAuthoringComponent]
+public struct TestSpawner : IComponentData
+{
+	public Entity Prefab;
+	public int3 Count;
+	public float3 Spacing;
+}
