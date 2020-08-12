7a36b68fee93429193a36a977222e2c250f8bb56
T.J. Martin <tjmartin@bungie.com>
2020-08-12 15:55:32 -0700

    Added plow finding

--- Ported/AutoFarmers/Assets/Scripts/WorkerComponents.cs
+++ Ported/AutoFarmers/Assets/Scripts/WorkerComponents.cs
@@ -1,4 +1,5 @@
 ﻿using Unity.Entities;
+using Unity.Mathematics;
 
 public struct WorkerIntent_Harvest : IComponentData { }
 public struct WorkerIntent_Sell : IComponentData
@@ -7,7 +8,10 @@ public struct WorkerIntent_Sell : IComponentData
 }
 
 public struct WorkerIntent_Plant : IComponentData { }
-public struct WorkerIntent_Plow : IComponentData { }
+public struct WorkerIntent_Plow : IComponentData
+{
+	public int2 TargetTilePos;
+}
 public struct WorkerIntent_Break : IComponentData { }
 
 
