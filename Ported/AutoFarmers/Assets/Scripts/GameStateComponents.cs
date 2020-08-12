41a3922e1fece63a6102207f01687931d6e78c57
Alexis Haraux <aharaux@bungie.com>
2020-08-12 12:52:27 -0700

    game state prefab + component, resource manager

---
+++ Ported/AutoFarmers/Assets/Scripts/GameStateComponents.cs
@@ -0,0 +1,7 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+public struct StoreTracking : IComponentData
+{
+	public float3 LastStorePosition;
+}
---
+++ Ported/AutoFarmers/Assets/Scripts/ResourceManagerAuthoring.cs
@@ -0,0 +1,11 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+[GenerateAuthoringComponent]
+public struct ResourceManager : IComponentData
+{
+	public int FarmerPrice;
+	public int DronePrice;
+	public int FarmerCoins;
+	public int DroneCoins;
+}
